d2f212458e5bc96c35628c8a13e3d5ed87b1e1c7
Esta SIMBIA <52277979+SmaeDev@users.noreply.github.com>
2025-11-10 15:03:17 +0100

    Update streamlit_app.py
    commented from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+#from snowflake.snowpark.functions import col
 
 # Write directly to the app
 streamlit.title(f":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
